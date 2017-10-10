d46cb1fb716f5fbfd0ec3d96cf93bfb384ca33fb
Daniel <daniel.jozefiuk@gmail.com>
2017-10-10 20:32:56 +0200

    Little RWD fix (XDD)

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,6 +6,7 @@
 
   <link rel="stylesheet" href="<?=base_url().'/assets/css/bootstrap.min.css'?>">
   <link rel="stylesheet" href="<?=base_url().'/assets/css/style.css'?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
